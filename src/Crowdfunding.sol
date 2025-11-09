f697a84967dd75e458fa88e011fc355646a1137d
dlaw-code <ajalaahmed8@gmail.com>
2025-11-09 08:20:49 +0100

    fix: correct ReentrancyGuard import path

--- src/Crowdfunding.sol
+++ src/Crowdfunding.sol
@@ -2,8 +2,7 @@
 pragma solidity ^0.8.21;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-// ...existing code...
-import {ReentrancyGuard} from "openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import {ICrowdfunding} from "src/interfaces/ICrowdfunding.sol";
 
 contract Crowdfunding is ReentrancyGuard, ICrowdfunding {
